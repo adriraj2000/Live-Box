03a951f09b5a201f6c8aa895882edee916169dc8
Adriraj <adrirajc@gmail.com>
2022-12-31 11:49:59 +0530

    coding the server part

---
+++ src/main/java/com/example/kafkachat/KafkachatApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkachatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkachatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/kafkachat/config/KafkaProducer.java
@@ -0,0 +1,36 @@
+package com.example.kafkachat.config;
+
+import com.example.kafkachat.model.Message;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducer {
+
+    @Bean
+    public ProducerFactory<String, Message> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(senderProps());
+    }
+
+    private Map<String,Object> senderProps() {
+        Map<String,Object> configurations = new HashMap<>();
+        configurations.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        configurations.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configurations.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return configurations;
+    }
+
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
